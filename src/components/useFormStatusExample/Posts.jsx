7089b72736968b206b3f756fca55ebd36a52454b
hqyuh <73837629+hqyuh@users.noreply.github.com>
2025-02-08 21:24:56 +0700

    fix error useFormStatus

--- src/components/useFormStatusExample/Posts.jsx
+++ src/components/useFormStatusExample/Posts.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { useFormStatus } from 'react-dom';
-import { useState } from 'react';
-import { useId } from 'react';
+import { useFormStatus } from "react-dom";
+import { useState, useId } from "react";
 
 // PostItem component
 const PostItem = ({ post }) => {
   return (
-    <div className='bg-blue-50 shadow-md p-4 my-6 rounded-lg'>
-      <h2 className='text-xl font-bold'>{post.title}</h2>
+    <div className="bg-blue-50 shadow-md p-4 my-6 rounded-lg">
+      <h2 className="text-xl font-bold">{post.title}</h2>
       <p>{post.body}</p>
     </div>
   );
@@ -22,11 +21,11 @@ const SubmitButton = () => {
 
   return (
     <button
-      className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-      type='submit'
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      type="submit"
       disabled={pending}
     >
-      {pending ? 'Submitting...' : 'Submit'}
+      {pending ? "Submitting..." : "Submit"}
     </button>
   );
 };
@@ -39,8 +38,8 @@ const PostForm = ({ addPost }) => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
     // We have direct access to the form data
     const newPost = {
-      title: formData.get('title'),
-      body: formData.get('body'),
+      title: formData.get(`${id}-title`),
+      body: formData.get(`${id}-body`),
     };
 
     addPost(newPost);
@@ -49,39 +48,39 @@ const PostForm = ({ addPost }) => {
   return (
     <form
       action={formAction}
-      className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
+      className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
     >
-      <div className='mb-4'>
+      <div className="mb-4">
         <label
-          className='block text-gray-700 text-sm font-bold mb-2'
+          className="block text-gray-700 text-sm font-bold mb-2"
           htmlFor={`${id}-title`}
         >
           Title
         </label>
         <input
-          className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id={`${id}-title`}
-          type='text'
-          placeholder='Enter title'
+          type="text"
+          placeholder="Enter title"
           name={`${id}-title`}
         />
       </div>
-      <div className='mb-6'>
+      <div className="mb-6">
         <label
-          className='block text-gray-700 text-sm font-bold mb-2'
+          className="block text-gray-700 text-sm font-bold mb-2"
           htmlFor={`${id}-body`}
         >
           Body
         </label>
         <textarea
-          className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id={`${id}-body`}
-          rows='5'
-          placeholder='Enter body'
+          rows="5"
+          placeholder="Enter body"
           name={`${id}-body`}
         ></textarea>
       </div>
-      <div className='flex items-center justify-between'>
+      <div className="flex items-center justify-between">
         <SubmitButton />
       </div>
     </form>
